3b8220559c61157d29b70a6b47f647335d267618
Mathias Presthagen <42730605+Meddiaz@users.noreply.github.com>
2020-04-28 21:38:41 +0200

    Update bears.js

--- bears.js
+++ bears.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 
+//  Queryet som brukes:
 const getBearsQuery = gql`
 {
   catalogue(path: "/teddy-bears", language: "en") {
@@ -24,7 +25,7 @@ const getBearsQuery = gql`
   }
 } 
 `
-
+//  Funksjonen vurderer resultatet fra GraphQL-spørringen, og spytter ut HTML-koden:
 function Bears() {
         const { loading, error, data } = useQuery(getBearsQuery);
         if (loading) return 'Loading';
@@ -32,19 +33,19 @@ function Bears() {
 
         console.log(data.catalogue.children);
 
-        var queryToHTML = data.catalogue.children.map(child => (
-          <div key={child.id} className='bearType'>
+        var queryToHTML = data.catalogue.children.map(child => (    //  Looper gjennom children-arrayet i katalogen, og binder verdiene til 'child'
+          <div key={child.id} className='bearType'> 
           
-            <h2>{child.name}</h2>
-            <div className='bearVariants'>
+            <h2>{child.name}</h2> 
+            <div className='bearVariants'>                       
             
-              {child.variants.map(variant => (
+              {child.variants.map(variant => (                      // Looper gjennom alle variantene av bjørnen, og renderer informasjonen som passende HTML-kode
                 <div className='bearVariant' key={variant.images[0].url}>
                   
                   <img src={variant.images[0].url} alt={child.name} className='bearPic'></img>
                   <h3>{variant.name}</h3>
                   {
-                    variant.stock < 6 && <p className='stockWarning'>Only {variant.stock} left</p>    // If the stock is less than 6, include warning
+                    variant.stock < 6 && <p className='stockWarning'>Only {variant.stock} left</p>    // Hvis beholdningen er under 6, vises det en advarsel
                   }
                   <h4>{variant.price}$</h4>
 
@@ -60,7 +61,7 @@ function Bears() {
         <div className = "catalogue">
                 <h1>{data.catalogue.name}</h1>
 
-                {queryToHTML}
+                {queryToHTML}   {/* Her printes resultet fra queryToHTML-varibalen, som vi genererte ovenfor */}
 
         </div>
         );
